537e4f6eaace0bf7ff98de325d3d458ce86e8e18
Erik Rodriguez <erik.rodrigu3z@gmail.com>
2017-11-23 00:43:33 -0700

    Add files via upload

---
+++ imagenBdCsharp/imagenBdCsharp/Form1.Designer.cs
@@ -0,0 +1,207 @@
+﻿namespace imagenBdCsharp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtId = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button4 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(336, 18);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(99, 22);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Buscar Imagen";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtId
+            // 
+            this.txtId.Location = new System.Drawing.Point(85, 18);
+            this.txtId.Name = "txtId";
+            this.txtId.Size = new System.Drawing.Size(245, 20);
+            this.txtId.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(18, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "ID";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 73);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Ubicacion";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 47);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(44, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Nombre";
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(85, 44);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(245, 20);
+            this.txtNombre.TabIndex = 5;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(279, 101);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(109, 23);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Guardar Imagen";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(411, 101);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(128, 23);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "Abrir Imagen De Bd";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(66, 48);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(277, 292);
+            this.pictureBox1.TabIndex = 8;
+            this.pictureBox1.TabStop = false;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 130);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(261, 388);
+            this.dataGridView1.TabIndex = 9;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(557, 101);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(128, 23);
+            this.button4.TabIndex = 10;
+            this.button4.Text = "Copiar Imagen";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackgroundImage = global::imagenBdCsharp.Properties.Resources.marco;
+            this.panel1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Location = new System.Drawing.Point(279, 130);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(406, 388);
+            this.panel1.TabIndex = 11;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(697, 530);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtId);
+            this.Controls.Add(this.button1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtId;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
+
---
+++ imagenBdCsharp/imagenBdCsharp/Form1.cs
@@ -0,0 +1,260 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace imagenBdCsharp
+{
+    /*la tabla para la bd
+     * 
+     CREATE TABLE imagen(
+	id_imagen int IDENTITY(1,1) NOT NULL,
+	imagen image NOT NULL,
+	nombre varchar(50) NULL     
+     */
+
+    public partial class Form1 : Form
+    {
+        //coection string
+        private const string Conectar = @"Server=(local);Database=DatosClientes;User Id=sa;Password=Ads720510.;";
+        SqlConnection cn = new SqlConnection(Conectar);
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            llenaGrid();
+        }
+
+        // boton buscar 
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog abrir = new OpenFileDialog();
+            abrir.Filter = "*.bmp;*.gif;*.jpg;*.png|*.bmp;*.gif;*.jpg;*.png";
+            abrir.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyPictures);
+            abrir.Title = "Selecciona La Imagen Que Se Guardará En La Base De Datos";
+            abrir.RestoreDirectory = true;
+
+            if (abrir.ShowDialog() == DialogResult.OK)
+            {
+                label2.Text = abrir.FileName;
+                txtNombre.Text = abrir.SafeFileName;
+
+                pictureBox1.Image = Image.FromFile(abrir.FileName);
+                pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+
+            }
+            else
+            {
+                label2.Text = "";
+                pictureBox1.Image = null;
+            }
+        }
+
+        //metodo para insertar imagen 
+        private void InsertarFotoBDD(int id, string fileFoto)
+        {
+            try
+            {
+                MemoryStream ms = new MemoryStream();
+                FileStream fs = new FileStream(fileFoto, FileMode.Open,FileAccess.Read,FileShare.ReadWrite);
+
+                ms.SetLength(fs.Length);
+                fs.Read(ms.GetBuffer(), 0, (int)fs.Length);
+
+                byte[] arrImg = ms.GetBuffer();
+                ms.Flush();
+                fs.Close();
+
+                using (SqlCommand cmd = cn.CreateCommand())
+                {
+                    cn.Open();
+
+                   // cmd.CommandText = "insert into imagen values(@id_imagen,@nombre,@imagen)";
+                    cmd.CommandText = "insert into imagen(nombre,imagen)values(@nombre,@imagen)";
+                    cmd.Parameters.Add("@id_imagen", SqlDbType.BigInt).Value = id;
+                    cmd.Parameters.Add("@nombre", SqlDbType.NVarChar, 50).Value = txtNombre.Text;
+                    //cmd.Parameters.Add("@nombre", SqlDbType.NVarChar,50).Value = Path.GetDirectoryName(fileFoto);
+                    cmd.Parameters.Add("@imagen", SqlDbType.VarBinary).Value = arrImg;
+
+                    cmd.ExecuteNonQuery();
+                    cn.Close();
+                    MessageBox.Show("Imagen Guardada","Guardar Imagen",MessageBoxButtons.OK,MessageBoxIcon.Exclamation);
+                }
+
+            }
+            catch (Exception e)
+            {
+
+                throw new Exception(e.Message);
+            }
+ 
+        }
+
+        //boton guardar imagen 
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string str = txtId.Text.Trim();
+            int Num;
+            bool isNum = int.TryParse(str, out Num);
+
+            //if (!isNum)
+            //{
+            //    MessageBox.Show("Falta id Foto", "Alerta", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            //    textBox1.Focus();
+            //}
+            //else
+            //{
+                if (pictureBox1.Image == null || label2.Text == "")
+                {
+                    MessageBox.Show("Falta Seleccionar Foto", "Alerta", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                }
+                else
+                {
+                    try
+                    {
+                        InsertarFotoBDD(Num, label2.Text);
+                        llenaGrid();
+                        txtNombre.ResetText();
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show(ex.Message, "Alerta", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+
+                }
+            //}
+        }
+
+        //metodo para mostrar imagen 
+        private Image ObtenerBitmapdeBDD(int id)
+        {
+           
+            try
+            {
+                using (SqlCommand cmd = cn.CreateCommand())
+                {
+                    cn.Open();
+                    
+                    cmd.CommandText = "select imagen from imagen where id_imagen = " + id + "";
+                    //cmd.Parameters.Add("@id", SqlDbType.BigInt).Value = id;
+
+                    byte[] arrImg = (byte[])cmd.ExecuteScalar();
+                    cn.Close();
+                    MemoryStream ms = new MemoryStream(arrImg);
+                    Image img = Image.FromStream(ms);
+
+                    ms.Close();
+
+                    return img;
+                }
+            }
+            catch (Exception e)
+            {                
+                throw new Exception(e.Message);
+            }
+        }
+
+        //mostrar imagen de BD
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string str = txtId.Text.Trim();
+            int Num;
+            bool isNum = int.TryParse(str, out Num);
+
+            if (!isNum)
+            {
+                MessageBox.Show("Falta id Foto", "Alerta", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                txtId.Focus();
+            }           
+            else
+            {
+                try
+                {
+                    label2.Text = "";
+                    txtId.Text = "";
+                    pictureBox1.Image = ObtenerBitmapdeBDD(Num);
+                    pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message, "Alerta", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                }
+
+            }           
+        }
+
+        //metodo para llenar grid
+        private void llenaGrid()
+        {
+            using (SqlCommand cmd = cn.CreateCommand())
+            {
+                try
+                {
+                    cn.Open();
+                    cmd.CommandText = "Select imagen,nombre from imagen";
+                    SqlDataAdapter da = new SqlDataAdapter(cmd);
+                    DataTable dt = new DataTable();
+                    da.Fill(dt);
+                    dataGridView1.DataSource = dt;
+                    cn.Close();                
+
+                }
+                catch (Exception e)
+                {
+
+                    throw new Exception(e.Message);
+                }
+
+            }
+        }
+
+        //evento click del grid
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            try
+            {
+                MemoryStream ms = new MemoryStream((byte[])dataGridView1.CurrentRow.Cells[0].Value);
+                pictureBox1.Image = Image.FromStream(ms);
+                pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            } 
+        }
+
+        // boton copiar imagen 
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (this.dataGridView1.GetCellCount(DataGridViewElementStates.Selected) > 0)
+            {
+                try
+                {
+                    // Add the selection to the clipboard.
+                    Clipboard.SetImage(pictureBox1.Image);
+                    MessageBox.Show("Imagen Copiada", "Copiar Imagen", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message,"Error",MessageBoxButtons.OK,MessageBoxIcon.Error);
+                }
+            }
+        }
+
+
+
+    }//// end class
+}
